e6a38b9c249f6bce85d9e96ab54f69b997787109
Louise Lee <louiseleezhiwei@gmail.com>
2017-02-27 17:46:01 -0500

    Finish up login page fxml

--- src/org/cs5431/GUI/LoginController.java
+++ src/org/cs5431/GUI/LoginController.java
@@ -6,11 +6,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
+
 
     @FXML
     private TextField txtUsername;
@@ -22,12 +24,18 @@ public class LoginController implements Initializable {
     private TextField txtServer;
 
     @FXML
+    private TextField txtPort;
+
+    @FXML
     private Button loginButton;
+
+    @FXML
+    public Text txtNoAcct;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         loginButton.setOnAction(e -> {
-            System.out.println("you clicked login");
+            tryLogin();
         });
 
         txtUsername.setOnKeyPressed(key -> {
@@ -44,8 +52,24 @@ public class LoginController implements Initializable {
 
         txtServer.setOnKeyPressed(key -> {
             if (key.getCode().equals(KeyCode.ENTER)) {
+                txtPort.requestFocus();
+            }
+        });
+
+        txtPort.setOnKeyPressed(key -> {
+            if (key.getCode().equals(KeyCode.ENTER)) {
                 loginButton.fire();
             }
         });
+
+        txtNoAcct.setOnKeyPressed(e -> {
+            //TODO
+            System.out.println("create new account");
+        });
+    }
+
+    private void tryLogin() {
+        //TODO
+        System.out.println("you clicked login");
     }
 }
