dcdf558a0246d4e3c41f7efbf38fb737105420b3
ruixin <ruixin@Janelles-MacBook-Pro.local>
2017-03-18 18:44:12 -0400

    authenticates, checks for unique username, and gets children of parent folder

--- src/org/cs5431_client/util/SQL_Connection.java
+++ src/org/cs5431_client/util/SQL_Connection.java
@@ -3,27 +3,64 @@ package org.cs5431_client.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class SQL_Connection {
 
     private static int port = 3306;
     private static String ip = "localhost";
+    private static String DB_USER = "admin";
+    private static String DB_PASSWORD = "$walaotwod0tseven$";
 
     public SQL_Connection(String ip, int port) {
         this.ip = ip;
         this.port = port;
     }
 
+    //TODO: make sure username if unique
+    private static  boolean isUniqueUsername(String username) {
+        String url = "jdbc:mysql://" + ip + ":" + Integer.toString(port) + "/cs5431";
+
+        System.out.println("Connecting to database...");
+
+        try (Connection connection = DriverManager.getConnection(url, DB_USER, DB_PASSWORD)) {
+            System.out.println("Database connected!");
+            PreparedStatement verifyUniqueness = null;
+
+            String checkUsername = "SELECT U.uid FROM Users U WHERE U.username = ?";
+            verifyUniqueness = connection.prepareStatement(checkUsername);
+
+            try {
+                verifyUniqueness.setString(1, username);
+                ResultSet rs = verifyUniqueness.executeQuery();
+                if (rs.next()) {
+                    System.out.println("The username is not unique");
+                    return false;
+                } else {
+                    System.out.println("The username is unique");
+                    return true;
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                if (verifyUniqueness != null) {
+                    verifyUniqueness.close();
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
     private static int createUser() {
 
         String url = "jdbc:mysql://" + ip + ":" + Integer.toString(port) + "/cs5431";
-        String username = "admin";
-        String password = "$walaotwod0tseven$";
 
-        System.out.println("Connecting database...");
+        System.out.println("Connecting to database...");
         int uid = 0;
 
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+        try (Connection connection = DriverManager.getConnection(url, DB_USER, DB_PASSWORD)) {
             System.out.println("Database connected!");
             PreparedStatement createUser = null;
             PreparedStatement createFolder = null;
@@ -61,7 +98,7 @@ public class SQL_Connection {
                 int folderid = rs.getInt(1);
                 createUser.setInt (1, 0);
                 createUser.setString (2, "myUsername");
-                createUser.setString (3, " myPassword");
+                createUser.setString (3, "myPassword");
                 createUser.setInt   (4, folderid);
                 createUser.setString (5, "myEmail");
                 createUser.setString    (6, "myPrivKey");
@@ -121,13 +158,12 @@ public class SQL_Connection {
     private static int createFso() {
 
         String url = "jdbc:mysql://" + ip + ":" + Integer.toString(port) + "/cs5431";
-        String username = "admin";
-        String password = "$walaotwod0tseven$";
 
-        System.out.println("Connecting database...");
+
+        System.out.println("Connecting to database...");
         int fsoid = 0;
 
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+        try (Connection connection = DriverManager.getConnection(url, DB_USER, DB_PASSWORD)) {
             System.out.println("Database connected!");
             PreparedStatement createFolder = null;
             PreparedStatement createLog = null;
@@ -144,10 +180,11 @@ public class SQL_Connection {
                 connection.setAutoCommit(false);
                 createFolder = connection.prepareStatement(insertFolder, Statement.RETURN_GENERATED_KEYS);
                 createLog = connection.prepareStatement(insertLog);
+                addPermission = connection.prepareStatement(insertEditor);
 
                 Timestamp currDate = new Timestamp(System.currentTimeMillis());
                 createFolder.setInt (1, 0);
-                createFolder.setInt (2, 1);
+                createFolder.setInt (2, 2);
                 createFolder.setString (3, "myFolderName");
                 createFolder.setString   (4, Integer.toString(10));
                 createFolder.setTimestamp (5, currDate);
@@ -163,7 +200,7 @@ public class SQL_Connection {
 
                 createLog.setInt(1, 0);
                 createLog.setInt (2, fsoid);
-                createLog.setInt(3, 2); //TODO: update UID
+                createLog.setInt(3, 2); //TODO: update uid
                 createLog.setTimestamp(4, currDate);
                 createLog.setString(5, "CREATE_FOLDER");
                 createLog.executeUpdate();
@@ -205,9 +242,86 @@ public class SQL_Connection {
         }
     }
 
+    private static int authenticate(String username, String encPwd) {
+
+        String url = "jdbc:mysql://" + ip + ":" + Integer.toString(port) + "/cs5431";
+
+        System.out.println("Connecting to database...");
+
+        try (Connection connection = DriverManager.getConnection(url, DB_USER, DB_PASSWORD)) {
+            System.out.println("Database connected!");
+            PreparedStatement verifyUser = null;
+
+            String checkPassword = "SELECT U.uid FROM Users U WHERE U.username = ? AND U.pwd = ?";
+            verifyUser = connection.prepareStatement(checkPassword);
+            //TODO: salting?
+
+            try {
+                verifyUser.setString(1, username);
+                verifyUser.setString(2, encPwd);
+                ResultSet rs = verifyUser.executeQuery();
+
+                if (rs.next()) {
+                    //user valid
+                    System.out.println("Valid user");
+                    int uid = rs.getInt(1);
+                    return uid;
+                }
+            } finally {
+                if (verifyUser != null) {
+                    verifyUser.close();
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        //TODO: log the number of failed authentications?
+        System.out.println("Invalid user");
+        return -1;
+    }
+
+    private static ArrayList<Integer> getChildren(int parentFolderid, int uid) {
+
+        ArrayList<Integer> files = new ArrayList<>();
+        String url = "jdbc:mysql://" + ip + ":" + Integer.toString(port) + "/cs5431";
+
+        System.out.println("Connecting to database...");
+
+        try (Connection connection = DriverManager.getConnection(url, DB_USER, DB_PASSWORD)) {
+            System.out.println("Database connected!");
+            PreparedStatement getFiles = null;
+
+            String selectFiles = "SELECT F.fsoid FROM FileSystemObjects F " +
+                    "WHERE F.parentFolderid = ? AND EXISTS" +
+                    "(SELECT * FROM Editors E WHERE E.uid=?) OR EXISTS" +
+                    "(SELECT * FROM Viewers V WHERE V.uid=?);";
+            getFiles = connection.prepareStatement(selectFiles);
+            //TODO: salting?
+
+            try {
+                getFiles.setInt(1, parentFolderid);
+                getFiles.setInt(2, uid);
+                getFiles.setInt(3, uid);
+                ResultSet rs = getFiles.executeQuery();
+                while (rs.next()) {
+                    files.add(rs.getInt(1));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (getFiles != null) {
+                    getFiles.close();
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return files;
+    }
+
     public static void main(String[] args) {
         //Connection connection = connectToDB();
-        System.out.print(createFso());
+        System.out.print(getChildren(2, 2));
     }
 
 }
