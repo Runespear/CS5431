403470095c389e0709df0c230c16e6ac544e47e1
Louise Lee <louiseleezhiwei@gmail.com>
2017-03-28 19:36:37 -0400

    Threadify everything in client

--- src/org/cs5431_client/controller/UserController.java
+++ src/org/cs5431_client/controller/UserController.java
@@ -1,5 +1,6 @@
 package org.cs5431_client.controller;
 
+import javafx.concurrent.Task;
 import org.cs5431_client.model.FileSystemObject;
 import org.cs5431_client.model.User;
 
@@ -31,9 +32,18 @@ public class UserController {
      * @param newPassword Password to be changed to.
      */
     public void changePassword(String oldPassword, String newPassword) {
-        //TODO: connect to server
-        //TODO: decide what it should return
-        //TODO: server side old password check
+        Task<Void> task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                //TODO: connect to server
+                //TODO: decide what it should return
+                //TODO: server side old password check
+                return null;
+            }
+        };
+        Thread th = new Thread(task);
+        th.setDaemon(true);
+        th.start();
     }
 
     /**
@@ -42,16 +52,40 @@ public class UserController {
      * @param newEmail Password to be changed to.
      */
     public void changeEmail(String oldEmail, String newEmail) {
-        //TODO: connect to server
-        //TODO: decide what it should return
-        //TODO: server side old email check
+        Task<Void> task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+            //TODO: connect to server
+            //TODO: decide what it should return
+            //TODO: server side old email check
+                return null;
+            }
+        };
+        Thread th = new Thread(task);
+        th.setDaemon(true);
+        th.start();
     }
 
     public List<FileSystemObject> getFileSystemObjects() {
-        int parentFolderId = user.getUserParentFolder().getId();
+        Task<List<FileSystemObject>> task = new Task<List<FileSystemObject>>() {
+            @Override
+            protected List<FileSystemObject> call() throws Exception {
+                int parentFolderId = user.getUserParentFolder().getId();
 
-        //TODO: query server to get all children associated with this folderId
-        //TODO: decrpyt file names
+                //TODO: query server to get all children associated with this folderId
+                //TODO: decrypt file names
+                return null;
+            }
+        };
+        final Object[] ret = {null};
+        task.setOnSucceeded(t -> ret[0] = task.getValue());
+        Thread th = new Thread(task);
+        th.setDaemon(true);
+        th.start();
+
+        if (ret[0] instanceof List<?>) {
+            return (List<FileSystemObject>) ret[0];
+        }
         return null;
     }
  }
