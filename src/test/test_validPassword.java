28af4736a4af13397c4bcdeeeb24b25872839f35
Wang, Zilong <zw243@cornell.edu>
2017-03-28 19:44:46 -0400

    Working on valid password test

--- src/test/test_validPassword.java
+++ src/test/test_validPassword.java
@@ -1,10 +1,21 @@
 package test;
 
+import org.cs5431_client.util.Validator;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Created by zilong on 28/3/17.
  */
 class test_validPassword {
-    
+    @Test
+    void test_validPassword_null(){
+        boolean empty = Validator.validPassword("");
+        boolean null1 = Validator.validPassword(null);
+
+        assertEquals(false, empty);
+        assertEquals(false, null1);
+
+    }
 }
