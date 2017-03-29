4d3f9057f0b5372bf38c5cdfc0ff50c4c723fb2b
Wang, Zilong <zw243@cornell.edu>
2017-03-28 21:52:38 -0400

    Testing valid filenames

---
+++ src/test/test_validFileName.java
@@ -0,0 +1,32 @@
+package test;
+
+import org.cs5431_client.util.Validator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by zilong on 28/3/17.
+ */
+class test_validFileName {
+    @Test
+    void test_validFileName_typical(){
+        String typical[] = {"cats","not adult", "assignment", "1","photos2007-7000"};
+        for (int i = 0; i < typical.length; i++){
+            boolean legit = Validator.validFileName(typical[i]);
+            assertEquals(true,legit);
+        }
+    }
+
+    @Test
+    void test_validFileName_invalidCharacters(){
+        boolean doubledots = Validator.validFileName("..");
+        boolean slashes = Validator.validFileName("/");
+        boolean upOne = Validator.validFileName("../");
+
+        assertEquals(false,doubledots);
+        assertEquals(false, slashes);
+        assertEquals(false, upOne);
+
+    }
+}
