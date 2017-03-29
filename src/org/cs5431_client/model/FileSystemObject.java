ae343b32674bd2bc694f052331613aaf4c6b29c0
Louise Lee <louiseleezhiwei@gmail.com>
2017-03-29 13:55:19 -0400

    Fix NPE caused by fixing findbugs bug

--- src/org/cs5431_client/model/FileSystemObject.java
+++ src/org/cs5431_client/model/FileSystemObject.java
@@ -25,7 +25,10 @@ public abstract class FileSystemObject {
         this.fileLog = new FileLog();
         //TODO: date modified set by DB timestamp
         //TODO: remove following line that's currently used for display purposes
-        this.lastModified = new Timestamp(lastModified.getTime());
+        if (lastModified != null)
+            this.lastModified = new Timestamp(lastModified.getTime());
+        else
+            this.lastModified = null;
     }
 
     public List<Integer> getEditors() {return this.editorIds; }
@@ -36,8 +39,10 @@ public abstract class FileSystemObject {
 
     public long getFileSize() {return size; }
 
-    public Timestamp getLastModified() {return new Timestamp(lastModified
-            .getTime()); }
+    public Timestamp getLastModified() {
+        if (lastModified == null)
+            return null;
+        return new Timestamp(lastModified.getTime()); }
 
     public int getId() {return id;}
 
