fe4772c0989d53d283b2b5303b8686e9b64968c1
Wang, Zilong <zw243@cornell.edu>
2017-03-28 20:03:09 -0400

    Password done

--- src/test/test_validPassword.java
+++ src/test/test_validPassword.java
@@ -16,6 +16,44 @@ class test_validPassword {
 
         assertEquals(false, empty);
         assertEquals(false, null1);
-
     }
+
+    @Test
+    void test_validPassword_tooShort(){
+        int starting = 33;
+        String pass = Character.toString( (char) starting );
+        boolean tooShort;
+        for (int i = 1; i < 16 ; i++){
+            //System.out.println(pass);
+            tooShort = Validator.validPassword(pass);
+            assertEquals(false,tooShort);
+            pass += Character.toString((char) (++starting));
+        }
+    }
+
+    @Test
+    void test_validPassword_lengthOK(){
+        int starting = 56;
+        String pass = "abcdefghijklmnop";
+        boolean justRight;
+        for (int i = 1; i < (999-14) ; i++){
+            //System.out.println(pass);
+            justRight = Validator.validPassword(pass);
+            assertEquals(true,justRight);
+            pass += Character.toString((char) (++starting));
+        }
+    }
+
+    @Test
+    void test_validPassword_length_TOOLONG(){
+        int starting = 1;
+        String pass = "";
+        boolean tooLong;
+        for (int i = 1; i<= 1000; i++){
+            pass+=Character.toString((char) (++starting));
+        }
+        tooLong = Validator.validPassword(pass);
+        assertEquals(false,tooLong);
+    }
+
 }
