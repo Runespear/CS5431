961a9fb40e24314c9b9f4825292efd01c58423ae
ngruixin <rn279@cornell.edu>
2017-05-02 21:03:42 -0400

    print lines for intrusion detection

--- cs5431-parent/cs5431-util/src/main/java/org/cs5431/IntrusionDetection.java
+++ cs5431-parent/cs5431-util/src/main/java/org/cs5431/IntrusionDetection.java
@@ -9,12 +9,62 @@ public class IntrusionDetection {
     private String ip;
     private String DB_USER;
     private String DB_PASSWORD;
+    private int FAILURE_PER_UID_PER_DAY = 20;
+    private int FAILURE_PER_IP_PER_DAY = 20;
+    private int TOTAL_FAILED_LOGIN_PER_DAY = 200;
 
     IntrusionDetection(String ip, int dbPort, String username, String password) {
         this.ip = ip;
         this.port = dbPort;
         this.DB_USER = username;
         this.DB_PASSWORD = password;
+
+        boolean isAttacked = false;
+
+        List<String> attackerIp = noPermissionAlert();
+        if (!attackerIp.isEmpty()) {
+            System.out.println("INTRUSION DETECTED. IP(s) that attempted to access the system without permission: " + attackerIp);
+            isAttacked = true;
+        }
+
+        attackerIp = getFailedAuthPerIp();
+        if (!attackerIp.isEmpty()) {
+            System.out.println("INTRUSION DETECTED. Multiple failed authentication attempts from the same IP(s): " + attackerIp);
+            isAttacked = true;
+        }
+
+        attackerIp = getFailedLoginsPerIp();
+        if (!attackerIp.isEmpty()) {
+            System.out.println("INTRUSION DETECTED. Multiple failed login attempts from the same IP(s): " + attackerIp);
+            isAttacked = true;
+        }
+
+        List<Integer> attackedUid = getAttemptedUidFailure();
+        if (!attackedUid.isEmpty()) {
+            System.out.println("INTRUSION DETECTED. Attempts to impersonate the following user(s): " + attackedUid);
+            isAttacked = true;
+        }
+
+        attackedUid = getFailedAuthPerUid();
+        if (!attackedUid.isEmpty()) {
+            System.out.println("INTRUSION DETECTED. Multiple failed authentication attempts on the same uid(s): " + attackedUid);
+            isAttacked = true;
+        }
+
+        attackedUid = getFailedLoginsPerUid();
+        if (!attackedUid.isEmpty()) {
+            System.out.println("INTRUSION DETECTED. Multiple failed login attempts on the same uid(s): " + attackedUid);
+            isAttacked = true;
+        }
+
+        if (totalFailedLogins() > TOTAL_FAILED_LOGIN_PER_DAY) {
+            System.out.println("INTRUSION DETECTED. An excessive number of failed login attempts.");
+            isAttacked = true;
+        }
+
+        if (!isAttacked) {
+            System.out.println("OK, no intrusion has been detected.");
+        }
     }
 
     //any ip from this list should be banned
@@ -60,9 +110,10 @@ public class IntrusionDetection {
                     "WHERE time_to_sec(timediff(NOW(), L.lastModified )) / 3600 < 24 AND L.status = \"FAILURE\" " +
                     "AND L.actionType = \"LOGIN\"" +
                     "GROUP BY L.sourceIp) as UL\n" +
-                    "WHERE UL.numFail > 20;";
+                    "WHERE UL.numFail > ?;";
             try {
                 getFailure = connection.prepareStatement(selectFailure);
+                getFailure.setInt(1, FAILURE_PER_IP_PER_DAY);
                 ResultSet rs = getFailure.executeQuery();
                 ArrayList<String> attackIp = new ArrayList<>();
                 while (rs.next()) {
@@ -94,9 +145,10 @@ public class IntrusionDetection {
                     "WHERE time_to_sec(timediff(NOW(), L.lastModified )) / 3600 < 24 AND L.status = \"FAILURE\" " +
                     "AND L.actionType = \"LOGIN\"" +
                     "GROUP BY L.uid) as UL\n" +
-                    "WHERE UL.numFail > 20;";
+                    "WHERE UL.numFail > ?;";
             try {
                 getFailure = connection.prepareStatement(selectFailure);
+                getFailure.setInt(1, FAILURE_PER_UID_PER_DAY);
                 ResultSet rs = getFailure.executeQuery();
                 ArrayList<Integer> attackedUid = new ArrayList<>();
                 while (rs.next()) {
@@ -166,6 +218,77 @@ public class IntrusionDetection {
             } finally {
                 if (getNoPermission != null) {
                     getNoPermission.close();
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    //Authentication comes from change password and delete user
+    List<String> getFailedAuthPerIp() {
+        String url = "jdbc:mysql://" + ip + ":" + Integer.toString(port) + "/PSFS5431?autoReconnect=true&useSSL=false";
+        PreparedStatement getFailure = null;
+
+        try (Connection connection = DriverManager.getConnection(url, DB_USER, DB_PASSWORD)) {
+
+            //currently 20 fails from the same ip per day
+            String selectFailure = "SELECT UL.sourceIp FROM\n" +
+                    "(SELECT L.sourceIp, COUNT(*) as numFail FROM UserLog L " +
+                    "WHERE time_to_sec(timediff(NOW(), L.lastModified )) / 3600 < 24 AND L.status = \"FAILURE\" " +
+                    "AND L.actionType = \"AUTHENTICATION\"" +
+                    "GROUP BY L.sourceIp) as UL\n" +
+                    "WHERE UL.numFail > ?;";
+            try {
+                getFailure = connection.prepareStatement(selectFailure);
+                getFailure.setInt(1, FAILURE_PER_IP_PER_DAY);
+                ResultSet rs = getFailure.executeQuery();
+                ArrayList<String> attackIp = new ArrayList<>();
+                while (rs.next()) {
+                    attackIp.add(rs.getString(1));
+                }
+                return attackIp;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                if (getFailure != null) {
+                    getFailure.close();
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    List<Integer> getFailedAuthPerUid() {
+        String url = "jdbc:mysql://" + ip + ":" + Integer.toString(port) + "/PSFS5431?autoReconnect=true&useSSL=false";
+        PreparedStatement getFailure = null;
+
+        try (Connection connection = DriverManager.getConnection(url, DB_USER, DB_PASSWORD)) {
+
+            //currently 20 fails from the same uid per day
+            String selectFailure = "SELECT UL.sourceIp FROM\n" +
+                    "(SELECT L.uid, COUNT(*) as numFail FROM UserLog L " +
+                    "WHERE time_to_sec(timediff(NOW(), L.lastModified )) / 3600 < 24 AND L.status = \"FAILURE\" " +
+                    "AND L.actionType = \"AUTHENTICATION\"" +
+                    "GROUP BY L.uid) as UL\n" +
+                    "WHERE UL.numFail > ?;";
+            try {
+                getFailure = connection.prepareStatement(selectFailure);
+                getFailure.setInt(1, FAILURE_PER_UID_PER_DAY);
+                ResultSet rs = getFailure.executeQuery();
+                ArrayList<Integer> attackedUid = new ArrayList<>();
+                while (rs.next()) {
+                    attackedUid.add(rs.getInt(1));
+                }
+                return attackedUid;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                if (getFailure != null) {
+                    getFailure.close();
                 }
             }
         } catch (SQLException e) {
