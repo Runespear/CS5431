09df35432ebc4fb3c11b1ca8a5cfe1f8b6a1b7bd
Zilong <wangzilongri@gmail.com>
2017-05-13 21:55:55 -0400

    SSS TODO: Reconstruction Check

---
+++ cs5431-parent/cs5431-util/src/test/java/SQL_Tests/SQL_AccountsTest.java
@@ -0,0 +1,29 @@
+package SQL_Tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by user on 17/4/2017.
+ */
+class SQL_AccountsTest {
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void test_createUser(){
+        //createUser
+    }
+
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+}
